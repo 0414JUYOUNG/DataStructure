decb1e1103dd34cc24903ab3f9c586db18ef1ab3
0414JUYOUNG <58586145+0414JUYOUNG@users.noreply.github.com>
2021-03-10 16:52:20 +0900

    Add files via upload

---
+++ 새 폴더/OneDim1.java
@@ -0,0 +1,14 @@
+package jana;
+
+public class OneDim1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+int sale[] = new int[] {1,2,3,4};
+for(int i=0; i<4; i++)
+	System.out.printf("%d/ºÐ±â : sale[%d] = ");
+	}
+	
+}
