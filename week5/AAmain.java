1ea4815900d74008d1de96c0752746f6312b724b
0414JUYOUNG <58586145+0414JUYOUNG@users.noreply.github.com>
2021-03-31 16:53:40 +0900

    Add files via upload

---
+++ week5/AAmain.java
@@ -0,0 +1,16 @@
+package week5;
+
+public class AAmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ListStack<String> stack = new ListStack<String>();
+		stack.push("apple"); stack.push("orange");stack.push("cherry");
+		System.out.println(stack.peek());
+		stack.push("pear"); stack.print();
+		stack.pop();  System.out.println(stack.peek());
+		stack.push("grape"); stack.print();
+	}
+
+}
